9f06bf0bc41b83de76dd3e75d76378bcac921238
Shirish Kumar <shirish.kumar@iiitg.ac.in>
2025-05-16 14:13:41 +0530

    Update build.bat

--- HelloWorldApp/Scripts/build.bat
+++ HelloWorldApp/Scripts/build.bat
@@ -1,9 +1,9 @@
 @echo off
 echo [BUILDING PROJECT]
 
-cd ..
 
-MSBuild.exe HelloWorldApp.sln /p:Configuration=Release /t:Rebuild /nologo > build_report.txt
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" ^
+HelloWorldApp.sln /p:Configuration=Release /t:Rebuild /nologo > build_report.txt
 IF %ERRORLEVEL% NEQ 0 (
     echo MSBuild failed. Check build_report.txt
     exit /b %ERRORLEVEL%
